e67d7d7c0ddde88dcf97b4f6e2b00d41ca6e8ad8
Eklavyasingh7 <codewitheklavya@gmail.com>
2026-05-22 10:17:47 +0530

    signup page frontend, fix cors error

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const expenseRoutes = require("./routes/expense.route");
-const authRoutes = require("./routes/auth.route")
+const authRoutes = require("./routes/auth.route");
+const cors = require('cors')
+
+
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/",(req,res)=>{
--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -1,2 +1,80 @@
+import { useState } from "react";
+import API from "../services/api";
 
+function Signup() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    
+
+    try {
+
+        console.log("button clicked")
+      const response = await API.post("/auth/signup", formData);
+
+      console.log(response.data);
+
+      alert("Signup successful");
+    } catch (error) {
+      console.log(error.response);
+
+      alert("Signup failed");
+      
+    }
+  };
+
+  return (
+    <div>
+      <h1>Signup</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter name"
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter email"
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter password"
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <button type="submit" className="cursor-pointer">Signup</button>
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
