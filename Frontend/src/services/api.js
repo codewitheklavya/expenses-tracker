5bfd7374f08fe86ad83572795e4e7accce48e2fa
Eklavyasingh7 <codewitheklavya@gmail.com>
2026-05-23 20:59:21 +0530

    backend connected with frontend

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-    baseURL: "http://localhost:3000/api"
+    baseURL: "https://expenses-tracker-ig38.onrender.com/api"
 });
 
 export default API;
